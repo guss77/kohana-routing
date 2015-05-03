66098273b283864c660de0a68cb0e693898d78c8
Oded Arbel <oded@geek.co.il>
2015-05-03 12:34:22 +0300

    Make 'defaults' optional (probably not useful, but I'm being inclusive here)

--- init.php
+++ init.php
@@ -4,5 +4,8 @@
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
-foreach (Kohana::$config->load('routes') as $name => $params)
-	Route::set($name, $params['uri'], @$params['rules'])->defaults($params['defaults']);
+foreach (Kohana::$config->load('routes') as $name => $params) {
+	$r = Route::set($name, $params['uri'], @$params['rules']);
+	if (@$params['defaults'])
+		$r->defaults($params['defaults']);
+}
